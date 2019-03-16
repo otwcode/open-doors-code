1736261d3d4aa1905cddc505bb0bba2ba0b87887
Ariana <ariana@astele.co.uk>
2019-03-16 16:33:36 +0000

    Only add default fandom if no other fandoms are listed

--- 06-Update-Tags-In-Story-Table.py
+++ 06-Update-Tags-In-Story-Table.py
@@ -1,18 +1,9 @@
 # encoding: utf-8
-import csv
-
-from eFiction import efiction
 from shared_python.Args import Args
 from shared_python.FinalTables import FinalTables
+from shared_python.PopulateTags import PopulateTags
 from shared_python.Sql import Sql
 from shared_python.Tags import Tags
-
-
-def valid_tags(key, tag_type_list):
-  return [d[key].strip() for d in tag_type_list
-          if key in d
-          and d[key] is not None
-          and d[key] != '']
 
 
 if __name__ == "__main__":
@@ -22,46 +13,6 @@ if __name__ == "__main__":
   sql = Sql(args, log)
   tags = Tags(args, sql.db, log)
   final = FinalTables(args, sql.db, log)
+  populate_tags = PopulateTags(args, sql.db, log, final)
 
-  if args.archive_type == 'EF':
-    table_names = efiction.table_names()
-  else:
-    table_names = {
-      'authors': 'authors',
-      'stories': 'stories',
-      'chapters': 'chapters'
-    }
-
-  log.info("Getting all tags per story...")
-  tags_by_story_id = tags.tags_by_story_id()
-  for (story_id, tags) in tags_by_story_id.items():
-
-    # group tags by type into comma-separated lists
-    # generate and run SQL to populate story table
-    from collections import defaultdict
-
-    tags_by_type = defaultdict(list)
-    for tag in tags:
-      tags_by_type[tag['ao3_tag_type']].append(tag)
-
-    story_tags = { 'categories': '', 'fandoms': '' }
-    categories = []
-    fandoms = [args.default_fandom]
-    for (tag_type, tag_type_tags) in tags_by_type.items():
-      if tag_type is None or tag_type == '':
-        log.warn("\nStory {2} has a None tag type\n {0} -> {1}".format(tag_type, tag_type_tags, story_id))
-      else:
-        tag_list = [d['ao3_tag'] for d in tag_type_tags if 'ao3_tag' in d and d['ao3_tag'] is not None]
-        categories += valid_tags('ao3_tag_category', tag_type_tags)
-        if tag_type == 'fandoms':
-          fandoms += tag_list
-        # Don't add the related fandom for this tag
-        # else:
-        #   fandoms += valid_tags('ao3_tag_fandom', tag_type_tags)
-        story_tags[tag_type] = ', '.join(set(tag_list))
-
-    story_tags['categories'] = ', '.join(set(categories))
-    story_tags['fandoms'] = ', '.join(set(fandoms))
-
-    final.populate_story_tags(story_id, 'stories', story_tags)
-    final.populate_story_tags(story_id, 'story_links', story_tags)
+  populate_tags.populate_tags()
---
+++ test/test_populate_tags.py
@@ -0,0 +1,57 @@
+from collections import defaultdict
+from unittest import TestCase
+import unittest
+
+from shared_python.FinalTables import FinalTables
+from shared_python.Logging import logger
+from shared_python.PopulateTags import PopulateTags
+from shared_python.Sql import Sql
+import argparse
+
+from shared_python.Tags import Tags
+
+
+def testArgs():
+  parser = argparse.ArgumentParser(description='Test an archive database')
+  args = parser.parse_args()
+  setattr(args, "archive_type", "EF")
+  setattr(args, "db_host", "localhost")
+  setattr(args, "db_user", "root")
+  setattr(args, "db_password", "")
+  setattr(args, "temp_db_database", "unit_test")
+  setattr(args, "output_database", "unit_test_output")
+  setattr(args, "default_fandom", "Fandom C (TV)")
+  return args
+
+class TestPopulate_tags(TestCase):
+  args = testArgs()
+  log = logger("test")
+  sql = Sql(args, log)
+  tags = Tags(args, sql.db, log)
+  final = FinalTables(args, sql.db, log)
+  populate_tags = PopulateTags(args, sql, log, tags, final)
+
+  basic_tags = {
+    'fandoms': [
+      {'original_tag': 'Fandom A', 'ao3_tag': 'Fandom A (TV)'},
+      {'original_tag': 'Fandom B', 'ao3_tag': 'Fandom B (TV)'}
+    ],
+    'tags': [
+      {'original_tag': 'a tag', 'ao3_tag': 'A Tag'}
+    ],
+    'rating': [{'original_tag': 'PG', 'ao3_tag': 'General Audiences'}]
+  }
+
+  def test_default_fandom_ignored_if_fandoms_present(self):
+    story_tags = self.populate_tags.tags_for_story(1, self.basic_tags)
+    self.assertEqual('Fandom A (TV), Fandom B (TV)', story_tags['fandoms'], 'Fandoms should be a comma-separated string of specified AO3 tags')
+
+  def test_default_fandom_used_if_no_fandoms_present(self):
+    tags_without_fandom = self.basic_tags.copy()
+    tags_without_fandom.pop('fandoms')
+    story_tags = self.populate_tags.tags_for_story(1, tags_without_fandom)
+    self.assertEqual('Fandom C (TV)', story_tags['fandoms'], 'Fandoms should be a comma-separated string of specified AO3 tags')
+
+
+if __name__ == '__main__':
+  unittest.main()
