805049f94b9889a5ca1c1d509fc46f1e6dfe9512
Simeng Sun <ssun@tcd.ie>
2021-10-24 22:49:32 -0400

    update OD692 test sql data

--- test/test_data/test.sql
+++ test/test_data/test.sql
@@ -29,7 +29,7 @@ CREATE TABLE tags (
 
 INSERT INTO tags VALUES (10,989,'original-tag-1','classes',NULL,'','','','',''),(11,345,'original-tag-2','classes',NULL,'','','','','');
 UE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) -- ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
